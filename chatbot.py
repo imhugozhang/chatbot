72dfc5194d510b0080f6062aacac21b0d0bcb778
imhugozhang <Imhugozhang@gmail.com>
2018-10-31 22:28:08 +1100

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,20 @@
-'''Trains a memory network on the bAbI dataset.
-References:
-- Jason Weston, Antoine Bordes, Sumit Chopra, Tomas Mikolov, Alexander M. Rush,
-  "Towards AI-Complete Question Answering: A Set of Prerequisite Toy Tasks",
-  http://arxiv.org/abs/1502.05698
-- Sainbayar Sukhbaatar, Arthur Szlam, Jason Weston, Rob Fergus,
-  "End-To-End Memory Networks",
-  http://arxiv.org/abs/1503.08895
-Reaches 98.6% accuracy on task 'single_supporting_fact_10k' after 120 epochs.
-Time per epoch: 3s on CPU (core i7).
-'''
-# Todo: different challenges, use tensorflow
-from __future__ import print_function
+# '''Trains a memory network on each of the 2 bAbI datasets.
+# References:
+# - Jason Weston, Antoine Bordes, Sumit Chopra, Tomas Mikolov, Alexander M. Rush,
+#   "Towards AI-Complete Question Answering: A Set of Prerequisite Toy Tasks",
+#   http://arxiv.org/abs/1502.05698
+# - Sainbayar Sukhbaatar, Arthur Szlam, Jason Weston, Rob Fergus,
+#   "End-To-End Memory Networks",
+#   http://arxiv.org/abs/1503.08895
+# '''
 
+#run pip install -r requirements.txt or pip3 install -r requirements.txt
+#to install dependencies
+
+from __future__ import print_function
 from tensorflow.keras import callbacks
 from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras import layers
 from tensorflow.keras.layers import Embedding, Input, Activation, Dense, Permute, Dropout, add, dot, concatenate, LSTM
 from tensorflow.keras.utils import get_file
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -24,25 +25,38 @@ import re
 import os
 from random import shuffle
 
+
+# configure challenge type here
+challenge_type = 'single_supporting_fact_10k'
+# challenge_type = 'two_supporting_facts_10k'
+
+# configure epoch here
+# in 'single_supporting_fact_10k', if epochs != 1, epochs = 120
+# in 'two_supporting_facts_10k', if epochs != 1, epochs = 40
+epochs = 40
+
 # when using Jupyter Notebook
 dir = os.getcwd()
 # when using local runtime
 dir = os.path.dirname(__file__)
 
+batch_size = None
+dropout = 0.3
+
 
 def tokenize(sent):
-    '''Return the tokens of a sentence including punctuation.
-    >>> tokenize('Bob dropped the apple. Where is the apple?')
-    ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
-    '''
     return [x.strip() for x in re.split(r'(\W+)?', sent) if x.strip()]
+
+# can also use nltk
+# from nltk.tokenize import word_tokenize
+# import nltk
+# nltk.download('punkt')
+# def tokenize(sent):
+#     return word_tokenize(sent)
 
 
 def parse_stories(lines, only_supporting=False):
-    '''Parse stories provided in the bAbi tasks format
-    If only_supporting is true, only the sentences
-    that support the answer are kept.
-    '''
+    '''Parse stories provided in the bAbi tasks format'''
     data = []
     story = []
     for line in lines:
@@ -55,11 +69,13 @@ def parse_stories(lines, only_supporting=False):
             q, a, supporting = line.split('\t')
             q = tokenize(q)
             substory = None
+            #If only_supporting is true, only the sentences
+            #that support the answer are kept.
             if only_supporting:
                 # Only select the related substory
                 supporting = map(int, supporting.split())
                 substory = [story[i - 1] for i in supporting]
-            else:
+            else: 
                 # Provide all the substories
                 substory = [x for x in story if x]
             data.append((substory, q, a))
@@ -71,16 +87,12 @@ def parse_stories(lines, only_supporting=False):
 
 
 def get_stories(f, only_supporting=False, max_length=None):
-    '''Given a file name, read the file,
-    retrieve the stories,
-    and then convert the sentences into a single story.
-    If max_length is supplied,
-    any stories longer than max_length tokens will be discarded.
-    '''
+    '''Given a file name, read the file, retrieve the stories'''
     data = parse_stories(f.readlines(), only_supporting=only_supporting)
-    # print(data)
 
     def flatten(data): return reduce(lambda x, y: x + y, data)
+    # convert the sentences into a single story
+    # If max_length is supplied, any stories longer than max_length tokens will be discarded.
     data = [(flatten(story), q, answer) for story, q,
             answer in data if not max_length or len(flatten(story)) < max_length]
     return data
@@ -93,10 +105,9 @@ def vectorize_stories(data, word_idx, story_maxlen, query_maxlen):
     for story, query, answer in data:
         x = [word_idx[w] for w in story]
         xq = [word_idx[w] for w in query]
-        # let's not forget that index 0 is reserved
+        # index 0 is reserved
         y = np.zeros(len(word_idx) + 1)
         y[word_idx[answer]] = 1
-        # y = [word_idx[answer]]
         X.append(x)
         Xq.append(xq)
         Y.append(y)
@@ -120,7 +131,7 @@ challenges = {
     # QA2 with 10,000 samples
     'two_supporting_facts_10k': 'tasks_1-20_v1-2/en-10k/qa2_two-supporting-facts_{}.txt',
 }
-challenge_type = 'two_supporting_facts_10k'
+
 challenge = challenges[challenge_type]
 
 print('Extracting stories for the challenge:', challenge_type)
@@ -175,111 +186,134 @@ print('answers: binary (1 or 0) tensor of shape (samples, vocab_size)')
 print('answers_train shape:', answers_train.shape)
 print('answers_test shape:', answers_test.shape)
 print('-')
-print('Compiling...')
+print('Building model...')
 
 
-epochs = 120
-batch_size = 32
-dropout = 0.3
-output_dim = 64
-LSTM_unit = 32
-if challenge_type == 'two_supporting_facts_10k':
-    batch_size = None
-    dropout = 0
-    output_dim = story_maxlen
-    LSTM_unit = story_maxlen/2
+def one_supporting_facts(epochs=epochs, batch_size=batch_size,
+                         dropout=dropout, output_dim=64,
+                         LSTM_unit=32):
+    
+    input_sequence = Input((story_maxlen,))
+    question = Input((query_maxlen,))
 
-# placeholders
-input_sequence = Input((story_maxlen,))
-question = Input((query_maxlen,))
+    input_encoder_m = Sequential()
+    input_encoder_m.add(Embedding(input_dim=vocab_size,
+                                  output_dim=output_dim))
+    input_encoder_m.add(Dropout(dropout))
+    
+    input_encoder_c = Sequential()
+    input_encoder_c.add(Embedding(input_dim=vocab_size,
+                                  output_dim=query_maxlen))
+    input_encoder_c.add(Dropout(dropout))
+    
+    question_encoder = Sequential()
+    question_encoder.add(Embedding(input_dim=vocab_size,
+                                   output_dim=output_dim,
+                                   input_length=query_maxlen))
+    question_encoder.add(Dropout(dropout))
+    
+    input_encoded_m = input_encoder_m(input_sequence)
+    input_encoded_c = input_encoder_c(input_sequence)
+    question_encoded = question_encoder(question)
 
-# encoders
-# embed the input sequence into a sequence of vectors
-input_encoder_m = Sequential()
-input_encoder_m.add(Embedding(input_dim=vocab_size,
-                              output_dim=output_dim))
-input_encoder_m.add(Dropout(dropout))
-# output: (samples, story_maxlen, embedding_dim)
+    match = dot([input_encoded_m, question_encoded], axes=(2, 2))
+    match = Activation('softmax')(match)
+    response = add([match, input_encoded_c])
+    response = Permute((2, 1))(response)
+    answer = concatenate([response, question_encoded])
+    answer = LSTM(LSTM_unit)(answer)  
 
-# embed the input into a sequence of vectors of size query_maxlen
-input_encoder_c = Sequential()
-input_encoder_c.add(Embedding(input_dim=vocab_size,
-                              output_dim=query_maxlen))
-input_encoder_c.add(Dropout(dropout))
-# output: (samples, story_maxlen, query_maxlen)
+    answer = Dropout(dropout)(answer)
+    answer = Dense(vocab_size)(answer)  
+    answer = Activation('softmax')(answer)
+    model = Model([input_sequence, question], answer)
+    return model
 
-# embed the question into a sequence of vectors
-question_encoder = Sequential()
-question_encoder.add(Embedding(input_dim=vocab_size,
-                               output_dim=output_dim,
-                               input_length=query_maxlen))
-question_encoder.add(Dropout(dropout))
-# output: (samples, query_maxlen, embedding_dim)
 
-# encode input sequence and questions (which are indices)
-# to sequences of dense vectors
-input_encoded_m = input_encoder_m(input_sequence)
-input_encoded_c = input_encoder_c(input_sequence)
-question_encoded = question_encoder(question)
+def two_supporting_facts(epochs=epochs, batch_size=batch_size,
+                         dropout=dropout, embed_hidden_size=50,
+                         sent_hidden_size=100,
+                         query_hidden_size=100):
+    sentence = layers.Input(shape=(story_maxlen,), dtype='int32')
+    encoded_sentence = layers.Embedding(
+        vocab_size, embed_hidden_size)(sentence)
+    encoded_sentence = layers.Dropout(dropout)(encoded_sentence)
 
-# compute a 'match' between the first input vector sequence
-# and the question vector sequence
-# shape: `(samples, story_maxlen, query_maxlen)`
-match = dot([input_encoded_m, question_encoded], axes=(2, 2))
-match = Activation('softmax')(match)
+    question = layers.Input(shape=(query_maxlen,), dtype='int32')
+    encoded_question = layers.Embedding(
+        vocab_size, embed_hidden_size)(question)
+    encoded_question = layers.Dropout(dropout)(encoded_question)
+    encoded_question = LSTM(embed_hidden_size)(encoded_question)
+    encoded_question = layers.RepeatVector(story_maxlen)(encoded_question)
 
-# add the match matrix with the second input vector sequence
-# (samples, story_maxlen, query_maxlen)
-response = add([match, input_encoded_c])
-response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
+    merged = layers.add([encoded_sentence, encoded_question])
+    merged = LSTM(embed_hidden_size)(merged)
+    merged = layers.Dropout(dropout)(merged)
+    preds = layers.Dense(vocab_size, activation='softmax')(merged)
+    model = Model([sentence, question], preds)
+    return model
 
-# concatenate the match matrix with the question vector sequence
-answer = concatenate([response, question_encoded])
 
-# the original paper uses a matrix multiplication for this reduction step.
-# we choose to use a RNN instead.
-answer = LSTM(LSTM_unit)(answer)  # (samples, 32)
+if challenge_type == 'single_supporting_fact_10k':
+    if epochs != 1:
+        epochs = 120
+    # filepath = os.path.join(dir, 'one_fact_chatbot_model.h5')
+    filepath = os.path.join(dir, 'one_fact_chatbot_model_weight.h5')
+    log_dir = os.path.join(dir, 'logs/1')
+    output_dim = 64
+    LSTM_unit = 32
+    model = one_supporting_facts(epochs=epochs, batch_size=batch_size,
+                                 dropout=dropout, output_dim=output_dim,
+                                 LSTM_unit=LSTM_unit)
+else:
+    if epochs != 1:
+        epochs = 40
+    filepath = os.path.join(dir, 'two_facts_chatbot_model_weight.h5')
+    log_dir = os.path.join(dir, 'logs/2')
+    embed_hidden_size = 20
+    sent_hidden_size = 100
+    query_hidden_size = 100
+    model = two_supporting_facts(dropout=dropout, batch_size=batch_size,
+                                 epochs=epochs,
+                                 embed_hidden_size=embed_hidden_size,
+                                 sent_hidden_size=sent_hidden_size,
+                                 query_hidden_size=query_hidden_size)
 
-# one regularization layer -- more would probably be needed.
-answer = Dropout(dropout)(answer)
-answer = Dense(vocab_size)(answer)  # (samples, vocab_size)
-# we output a probability distribution over the vocabulary
-answer = Activation('softmax')(answer)
 
-# build the final model
-model = Model([input_sequence, question], answer)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
+model.compile(optimizer='adam', loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-filepath = os.path.join(dir, 'chatbot_model.h5')
-log_dir = os.path.join(dir, 'logs/1')
+
 # make the file directories needed
 new_dir = os.path.join(dir, 'logs')
 if not os.path.exists(new_dir):
     os.makedirs(new_dir)
 
-# train
-callbacks = [
-    # callbacks.ModelCheckpoint(filepath=filepath, verbose=1,
-    #                           monitor='val_loss', save_best_only=True),
-    # Check out the train history later with Tensorboard
-    callbacks.TensorBoard(log_dir=log_dir),
-    callbacks.EarlyStopping(patience=10)]
+# # train
+# print('Training...')
+# callbacks = [
+#     callbacks.ModelCheckpoint(filepath=filepath, verbose=1,
+#                               monitor='val_loss', save_best_only=True),
+#     # Check out the train history later with Tensorboard
+#     callbacks.TensorBoard(log_dir=log_dir),
+#     callbacks.EarlyStopping(patience=20)]
 
-model.fit([inputs_train, queries_train], answers_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_split=0.1,
-          callbacks=callbacks)
+# model.fit([inputs_train, queries_train], answers_train,
+#           batch_size=batch_size,
+#           epochs=epochs,
+#           validation_split=0.1,
+#           callbacks=callbacks)
 
-# # evaluate
-# test_result = model.evaluate([inputs_test, queries_test], answers_test)
-# print(
-#     f'Test result:\nTest loss = {test_result[0]}, Test accuracy = {test_result[1]}')
+# del model
+# model = load_model(filepath=filepath)
+model.load_weights(filepath=filepath)
+
+# evaluate
+test_result = model.evaluate([inputs_test, queries_test], answers_test)
+print(
+    f'Test result:\nTest loss = {test_result[0]}, Test accuracy = {test_result[1]}')
 
 # predict
-del model
-model = load_model(filepath=filepath)
 predictions = model.predict([inputs_test, queries_test])
 re_word_idx = {v: k for k, v in word_idx.items()}
 
@@ -291,39 +325,3 @@ for i in range(9):
             print('\nGround truth: ' + ''.join(k))
     predicted = re_word_idx[np.argmax(predictions[i])]
     print(f'Prediction  : {predicted}\n')
-
-
-'''
-# import en_core_web_sm
-# def vectorize_stories(data):
-#     X = []
-#     Xq = []
-#     Y = []
-#     nlp = en_core_web_sm.load()
-#     i=1
-
-#     for story, query, answer in data:
-#         x = [nlp(w).vector for w in story]
-#         xq = [nlp(w).vector for w in query]
-#         y = [nlp(w).vector for w in answer]
-#         X.append(x)
-#         Xq.append(xq)
-#         Y.append(y)
-#         i+=1
-#         print(i)
-
-#     X = np.array(X)
-#     Xq = np.array(Xq)
-#     Y = np.array(Y)
-#     return X, Xq, Y
-
-
-# def tokenize(sent):
-#     nlp = en_core_web_sm.load()
-#     doc = nlp(sent)
-#     sentence = []
-#     for token in doc:
-#         sentence.append(token.text)
-#     return sentence
-# print(tokenize('Bob dropped the apple. Where is the apple?'))
-'''
